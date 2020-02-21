0b93c022c0e7a16ff94c118c71f3a74ca77ec26d
Md Saney Alam <codecamper@outlook.com>
2020-02-21 13:06:25 -0500

    Node MongoDB Example 2

---
+++ nucamp-node-express/node-mongo/operations.js
@@ -0,0 +1,17 @@
+const assert = require('assert').strict;
+
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+};
+
+exports.findDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+};
+
+exports.updateDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+};
